5ed6553539b08b7eb722847bd389eff8ed91c46b
Zander Huff <z@zanderhuff.io>
2020-01-31 14:39:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nowServing(katzDeliLine) {
     while (i < katzDeliLine.length) {
       i++
     }
-    return `Currently serving ${katzDeliLine.shift()}.`
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
 
