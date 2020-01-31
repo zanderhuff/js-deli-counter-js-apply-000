fa03140b8d592f3d44e12dcb3b6b97ba231f18f4
Zander Huff <z@zanderhuff.io>
2020-01-31 14:22:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (x === 0){
+    for (var i = 0; i < katzDeliLine.length; i++){
+      push.array(katzDeliLine[i])
+      return array;
+      remove.array(katzDeliLine[i])
+    }
+    return array;
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(katzDeliLine) {
+  
+}
